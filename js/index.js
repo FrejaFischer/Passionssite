6f3598a0e678dd89788b54b023843e1781101600
Freja Fischer <frej657e@stud.kea.dk>
2023-02-20 13:47:17 +0100

    category virker (!!) altså nogenlunde...

--- js/index.js
+++ js/index.js
@@ -10,12 +10,93 @@ function slikfixVelkommen() {
 
   document.querySelector("#welcome_img").classList.add("zoom_out");
 
-  document.querySelector("#welcome_img").addEventListener("animationend", produktSiden);
+  document.querySelector("#welcome_img").addEventListener("animationend", fetchProducts);
+
+  document.querySelector(`[data-category="karamel"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="vingummier"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="surt"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="chokolade"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="lakrids"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="skum"]`).addEventListener("click", fetchCandy);
 }
 
-function produktSiden() {
-  console.log("produktSiden");
+function fetchProducts() {
   document.querySelector("#welcome_img").classList.add("hide");
+  document.querySelector("#categori_section").classList.remove("hide");
+  document.querySelector("#categori_section").classList.add("appear");
+
+  fetch("https://blandselvslik-8d50.restdb.io/rest/blandselvslik", {
+    method: "get",
+    headers: {
+      "x-apikey": "63ecc171478852088da682f2",
+    },
+  })
+    .then((e) => e.json())
+    .then(showProducts);
+}
+
+function showProducts(products) {
+  console.log("showProducts");
+  //looper og kalder showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  console.log("showProduct");
+  //fang template
+  const template = document.querySelector("#candy_product").content;
+  //lav en kopi
+  const copy = template.cloneNode(true);
+  //ændre indhold
+  copy.querySelector("h2").textContent = product.name;
+
+  //appende
+  document.querySelector("#products").appendChild(copy);
+
+  document.querySelector("#products").classList.remove("hide");
+  document.querySelector("#products").classList.add("appear");
+}
+
+function fetchCandy(evt) {
+  console.log(evt.target.dataset.category);
+
+  document.querySelector("#products").remove();
+
+  fetch(`https://blandselvslik-8d50.restdb.io/rest/blandselvslik?q={"category":"${evt.target.dataset.category}"}`, {
+    method: "get",
+    headers: {
+      "x-apikey": "63ecc171478852088da682f2",
+    },
+  })
+    .then((e) => e.json())
+    .then(showGummies);
+}
+
+function showGummies(products) {
+  console.log("showGummies");
+  products.forEach(showGummi);
+}
+
+function showGummi(product) {
+  console.log("showGummi");
+  //fang template
+  const template = document.querySelector("#candy_product").content;
+  //lav en kopi
+  const copy = template.cloneNode(true);
+  //ændre indhold
+  copy.querySelector("h2").textContent = product.name;
+
+  //appende
+  document.querySelector("#products2").appendChild(copy);
+
+  document.querySelector("#products2").classList.add("appear");
+
+  document.querySelector(`[data-category="karamel"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="vingummier"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="surt"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="chokolade"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="lakrids"]`).addEventListener("click", fetchCandy);
+  document.querySelector(`[data-category="skum"]`).addEventListener("click", fetchCandy);
 }
 
 //https://blandselvslik-8d50.restdb.io/rest/blandselvslik?h={%22$fields%22:{%22category%22:1}}
--- js/productlist.js
+++ js/productlist.js
@@ -1,1 +1,48 @@
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category");
 
+fetch("https://blandselvslik-8d50.restdb.io/rest/blandselvslik?filter=" + category, {
+  method: "get",
+  headers: {
+    "x-apikey": "63ecc171478852088da682f2",
+  },
+})
+  .then((e) => e.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  //looper og kalder showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //fang template
+  const template = document.querySelector("#product_item_template").content;
+  //lav en kopi
+  const copy = template.cloneNode(true);
+  //ændre indhold
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  copy.querySelector("h2").textContent = product.productdisplayname;
+  copy.querySelector(".subcategory").textContent = product.subcategory;
+  copy.querySelector(".brand").textContent = product.brandname;
+
+  copy.querySelector("#price").textContent = "DKK " + product.price + ",-";
+
+  if (product.discount !== null) {
+    copy.querySelector("#new_price").textContent = "DKK " + Math.trunc((product.price / 100) * product.discount) + ",-";
+
+    copy.querySelector("#price").classList.add("price_change");
+  } else {
+    copy.querySelector("#new_price").classList.add("hide");
+  }
+
+  if (product.soldout == false) {
+    copy.querySelector("#soldout").classList.add("hide");
+  } else {
+    copy.querySelector("img").classList.add("gone");
+  }
+
+  copy.querySelector(".link_product").setAttribute("href", `product.html?id=${product.id}`);
+  //appende
+  document.querySelector("#product_list").appendChild(copy);
+}
